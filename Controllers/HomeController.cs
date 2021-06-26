3eed61bd64efed7abe07cb75bb162a0afc6bfc28
sarovar <iamsarovarswing@gmail.com>
2021-06-26 12:23:47 +0530

    initial mvc set up

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCBookStore.Controllers
+{
+    public class HomeController: Controller
+    {
+
+        public string Index()
+        {
+            return " Hello from home controller";
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -16,25 +16,40 @@ namespace MVCBookStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddControllersWithViews(); // enable mvc design pattern into our .netcore app
+
         }
+
+
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                app.UseDeveloperExceptionPage(); // inserts this middleware
             }
 
-            app.UseRouting();
+
+            /*app.Use(async (context, next) =>
+            {
+
+                await context.Response.WriteAsync("This is a middleware");
+                await next(); // passes control to the next middleware n the pipeline                
+            });*/
+
+            app.UseRouting(); // maps the url to a particular resource, ie. enable routing
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute(); // default route {controller=Home}/{action=Index}/{id?}
+
+
             });
+
+          
         }
     }
 }
